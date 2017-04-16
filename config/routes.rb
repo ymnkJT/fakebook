3be4b3acbe2b66d01400c032d02cd6b7001b964c
ymnkJT <javadtomi@gmail.com>
2017-04-16 13:55:04 +0900

    Set letter opener for development environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'topics#index'
   devise_for :users
-
   resources :topics, only: [:index, :create, :edit, :update, :destroy]
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
