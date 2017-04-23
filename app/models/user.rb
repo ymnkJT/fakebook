3ab05adffb035c5a862dd869181c1db03fb565d2
ymnkJT <javadtomi@gmail.com>
2017-04-23 16:11:02 +0900

    Set profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          :omniauthable
+  mount_uploader :avatar, AvatarUploader
   has_many :topics
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
@@ -42,5 +43,14 @@ class User < ActiveRecord::Base
 
   def self.create_unique_string
     SecureRandom.uuid
+  end
+
+  def update_with_password(params, *options)
+    if provider.blank?
+      super
+    else
+      params.delete :current_password
+      update_without_password(params, *options)
+    end
   end
 end
