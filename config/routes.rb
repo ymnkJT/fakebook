bfcb732a28a4a47875fb3d8c6757f46ec5038780
ymnkJT <javadtomi@gmail.com>
2017-05-05 11:56:47 +0900

    Create a function of messages

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :relationships, only: [:create, :destroy]
 
+  resources :conversations do
+    resources :messages
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
